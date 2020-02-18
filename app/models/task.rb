9a0f86fbf9a745185b9bdaa0bb04c8ef46cb0868
deelye <dee.yen.lye@gmail.com>
2020-02-18 15:55:01 +0000

    challenge complete at bare minimum level

---
+++ app/models/task.rb
@@ -0,0 +1,5 @@
+class Task < ApplicationRecord
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Show all
+  get 'tasks', to: 'tasks#index'
+  # Create new
+  get 'tasks/new', to: 'tasks#new'
+  # Show task
+  get 'tasks/show/:id', to: 'tasks#show', as: 'task'
+  # Update db
+  post 'tasks', to: 'tasks#create'
+  # Edit task
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  # Update db
+  patch 'tasks/show/:id', to: 'tasks#update'
+  # Delete task
+  delete 'tasks/show/:id', to: 'tasks#destroy'
 end
